e91ea9461a9cb3aff2b3e95eb295aa72815163f0
Bilbo3340 <vladik.elce1@gmail.com>
2022-08-31 11:04:55 +0300

    upt

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int d = year % 4;
+        int d1 = year % 100;
+        int d2 = year % 400;
+        switch (d) {
+            case 0: {
+                if (d1 == 0) {
+                    if (d2 == 0) {
+                        System.out.println("leap");
+                    } else {
+                        System.out.println("not leap");
+                    }
+                } else {
+                    System.out.println("leap");
+                }
+                break;
+            }
+            default: System.out.println("not leap");
+        }
     }
 }
